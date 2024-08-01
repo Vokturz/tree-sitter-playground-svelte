d10b6a9b84ced96695d4c4f8bc4fe80213d6952f
Vokturz <victornavarrokp47@gmail.com>
2024-08-01 01:56:53 -0400

    Add Tree-sitter logo and style the header section

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,33 @@
 <script>
   import Parser from './lib/Parser.svelte'
+  import treeSitterLogo from './assets/tree-sitter-small.png'
 </script>
+
+<style>
+.header {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.logo {
+  margin-right: 15px;
+}
+
+.logo img {
+  height: 100px; /* Adjust the height as needed */
+  width: auto;
+}
+</style>
 
 <main>
 
+  <div class="header">
+    <div class="logo">
+      <img src={treeSitterLogo} alt="Tree Sitter Logo" />
+    </div>
+    <h1>Tree-sitter Playground</h1>
+  </div>
   <div>
     <Parser />
   </div>
